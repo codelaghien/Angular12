a56024da814cb257ad703c1419a57da20a601ad5
codelaghien <codelaghien.github.io>
2021-05-30 13:22:59 -0700

    ngFor Ap Dung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,16 @@ export class HomeComponent implements OnInit {
   public name = 'Huy Nguyễn';
   public age = 25;
   public traiCay = ['Táo', 'Nho', 'Cam', 'Quít', 'Ổi'];
+  public traiCay2 = [
+    { ten: 'Táo', gia: 12, haGia: true },
+    { ten: 'Nho', gia: -3.234, haGia: false },
+    { ten: 'Cam', gia: 34.26, haGia: false },
+    { ten: 'Quít', gia: 32.45, haGia: true },
+  ];
 
   constructor() {}
 
   public ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay);
+    console.log('trai cay = ', this.traiCay2);
   }
 }
