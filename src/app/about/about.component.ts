cb0daeabb34a1b13f501877745a71f6988f24145
codelaghien <codelaghien.github.io>
2021-05-31 11:52:16 -0700

    Directive

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HighLightDirective } from '../Directives/high-light.directive';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   public loginName = 'admin';
+  public myColor = 'red';
 
   constructor() {}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { OnSalePipe } from './on-sale.pipe';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HighLightDirective } from './Directives/high-light.directive';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, OnSalePipe, PageNotFoundComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    OnSalePipe,
+    PageNotFoundComponent,
+    HighLightDirective,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
