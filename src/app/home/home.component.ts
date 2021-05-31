b4bc3bb215bad4cdb17784afb3f43bf37aef27ac
codelaghien <codelaghien.github.io>
2021-05-31 09:33:43 -0700

    ngFor - Combobox - City - District

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,10 +15,87 @@ export class HomeComponent implements OnInit {
     { ten: 'Cam', gia: 34.26, haGia: false },
     { ten: 'Quít', gia: 32.45, haGia: true },
   ];
+  public districts: string[] = ['Quận Huyện'];
+  public vietnamData = [
+    { city: 'Chọn thành phố', district: ['Quận Huyện'] },
+    {
+      city: 'An Giang',
+      district: [
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Thị xã Tân Châu',
+        'Huyện An Phú',
+        'Huyện Châu Phú',
+        'Huyện Châu Thành',
+        'Huyện Chợ Mới',
+        'Huyện Phú Tân',
+        'Huyện Thoại Sơn',
+        'Huyện Tịnh Biên',
+        'Huyện Tri Tôn',
+      ],
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district: [
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Thị xã Phú Mỹ',
+        'Huyện Châu Đức',
+        'Huyện Côn Đảo',
+        'Huyện Đất Đỏ',
+        'Huyện Long Điền',
+        'Huyện Tân Thành',
+        'Huyện Xuyên Mộc',
+      ],
+    },
+    {
+      city: 'Bạc Liêu',
+      district: [
+        'Thành phố Bạc Liêu',
+        'Huyện Đông Hải',
+        'Huyện Giá Rai',
+        'Huyện Hòa Bình',
+        'Huyện Hồng Dân',
+        'Huyện Phước Long',
+        'Huyện Vĩnh Lợi',
+      ],
+    },
+    {
+      city: 'Bắc Kạn',
+      district: [
+        'Thị xã Bắc Kạn',
+        'Huyện Ba Bể',
+        'Huyện Bạch Thông',
+        'Huyện Chợ Đồn',
+        'Huyện Chợ Mới',
+        'Huyện Na Rì',
+        'Huyện Ngân Sơn',
+        'Huyện Pác Nặm',
+      ],
+    },
+  ];
 
   constructor() {}
 
   public ngOnInit(): void {
-    console.log('trai cay = ', this.traiCay2);
+    console.log('vietnamData = ', this.vietnamData);
+  }
+
+  public changeCity(event: any): void {
+    const city = event.target.value;
+    if (!city) {
+      return;
+    }
+
+    // cách 1
+    // const search = this.vietnamData.filter((data) => data.city === city);
+    // console.log('search', search);
+    // if (search && search.length > 0) {
+    //   this.districts = search[0].district;
+    // }
+
+    // cách 2
+    this.districts =
+      this.vietnamData.find((data) => data.city === city)?.district || [];
   }
 }
